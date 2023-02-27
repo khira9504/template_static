a6cff3ae6e96078fb94770222581b8c6e7b9ce0d
keiyahiraizumi <k.hiraizumi@ooparts-co.asia>
2023-02-27 18:04:27 +0900

    add

---
+++ test/scripts/ts/index.ts
@@ -0,0 +1,5 @@
+const btn = document.getElementById("btn");
+const numberWrap = document.getElementById("number") as HTMLElement;
+// const numberValue = numberWrap.value;
+console.log("ok");
+console.log(numberWrap);
